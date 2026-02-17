d982d6df250f0a4efeb0965649376fc65fc28411
Debarshee Chakraborty <debarsheechakraborty.11d@gmail.com>
2026-02-17 11:30:28 +0530

    Created the talks page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./pages/home/home";
 import Links from "./pages/links/links";
 import Meet from "./pages/meet/meet";
 import Blogs from "./pages/blogs/blogs";
+import Talks from "./pages/talks/talks";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blogs" element={<Blogs />} />
+        <Route path="/talks" element={<Talks />} />
         <Route path="/links" element={<Links />} />
         <Route path="/meet" element={<Meet />} />
       </Routes>
