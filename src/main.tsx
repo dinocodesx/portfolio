95e67e4149b7ed70e68134a0f6843f1e791a6725
Debarshee Chakraborty <debarsheechakraborty.11d@gmail.com>
2026-05-27 06:20:47 +0530

    Removed Junk

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,6 @@ import App from "@/App";
 import "@/index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { registerSW } from "virtual:pwa-register";
-
-registerSW({ immediate: true });
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
