3a0c0a4a8d2f5dfe5bc2b19b0d9ffa6cd41e0679
Debarshee Chakraborty <debarsheechakraborty.11d@gmail.com>
2026-05-27 06:02:13 +0530

    Made links page look cool

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import App from "@/App";
 import "@/index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { registerSW } from "virtual:pwa-register";
+
+registerSW({ immediate: true });
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,56 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig, loadEnv } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", "");
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [
+      react(),
+      tailwindcss(),
+      VitePWA({
+        registerType: "autoUpdate",
+        includeAssets: ["favicon.png", "robots.txt", "assets/**/*"],
+        manifest: {
+          name: "Debarshee Chakraborty Portfolio",
+          short_name: "DinoCodes",
+          description: "Portfolio of Debarshee Chakraborty",
+          theme_color: "#000000",
+          icons: [
+            {
+              src: "favicon.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "favicon.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+          ],
+        },
+        workbox: {
+          globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+          runtimeCaching: [
+            {
+              urlPattern: /^https:\/\/baos70mga7\.ufs\.sh\/.*/i,
+              handler: "CacheFirst",
+              options: {
+                cacheName: "external-images-cache",
+                expiration: {
+                  maxEntries: 50,
+                  maxAgeSeconds: 60 * 60 * 24 * 30, // 30 Days
+                },
+                cacheableResponse: {
+                  statuses: [0, 200],
+                },
+              },
+            },
+          ],
+        },
+      }),
+    ],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
